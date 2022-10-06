ec5b631b8326d4e3c48509c3ad31b8f5220f6069
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-05 22:48:52 -0500

    Added a user/computer counter, and a round system.

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
 import random
 
-# user_input = input("Let's play rock, paper, and scissors. Yes? Y/N ")
-user_input = input("Pick rock, paper, or scissors: ")
+computer_score = 0
+user_score = 0
+i = 0
+chance = int(input("How manys time you want to play (no. of chances)?: "))
 
-# if user_input.lower() == 'y':
-#     print("Okay, here we go. ")
-# else:
-#     print("You don't want to play? ")
+while i <= chance:
 
-possible_options = ["Rock", "Paper", "Scissors"]
-computer_option = random.choice(possible_options)
+    user_input = input("\nPick Rock, Paper, or Scissors: ").lower()
 
-# start = input("You'll play against a computer. Pick first: ")
-print(f"\nYou chose {user_input}, computer chose {computer_option}.\n")
+    possible_options = ["Rock", "Paper", "Scissors"]
+    computer_option = random.choice(possible_options).lower()
 
-if user_input == computer_option:
-    print(f"Both players selected {user_input}. It's a tie!")
-elif user_input == "rock":
-    if computer_option == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_input == "paper":
-    if computer_option == "rock":
-        print("Paper convers rock! You win.")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_input == "Scissors":
-    if computer_option == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+    if user_input == computer_option:
+        print("You both tied!")
+    elif user_input == "rock":
+        if computer_option == "scissors":
+            user_score += 1
+            print("Rock smashes Scissors! You win!")
+        else:
+            computer_score += 1
+            print("Paper covers rock! You lose.")
+    elif user_input == "paper":
+        if computer_option == "rock":
+            user_score += 1
+            print("Paper convers rock! You win.")
+        else:
+            computer_score += 1
+            print("Scissors cuts paper! You lose.")
+    elif user_input == "scissors":
+        if computer_option == "paper":
+            user_score += 1
+            print("Scissors cuts paper! You win!")
+        else:
+            computer_score += 1
+            print("Rock smashes scissors! You lose.")
+
+    print(
+        f"You chose {user_input} - Points: {user_score}, computer chose {computer_option} - Points: {computer_score}.\n")
+    
+    play_again = input("Want to play again: (y/n) ")
+    if play_again.lower() != 'y':
+        break
+
+    i += 1
